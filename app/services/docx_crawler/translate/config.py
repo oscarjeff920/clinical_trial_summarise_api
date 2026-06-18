533588b8f7670b4e76448770954e00b1c3f95fdc
Oscar Jeffery <oscarjjeff920@gmail.com>
2026-06-18 14:31:07 +0100

    updated to snake case to keep consistency

--- app/services/docx_crawler/translate/config.py
+++ app/services/docx_crawler/translate/config.py
@@ -1,6 +1,6 @@
 OUTPUT_SENTENCES = {
     "totals": "A total of {C1N} participants received {C1}, and a total of {C2N} participants received {C2}.",
-    "per-term": {
+    "per_term": {
         "both": "More participants who received {C1} ({C1%}) experienced {T} compared to {C2} ({C2%}).",
         "equal": "An equal proportion of participants experienced {T} ({C1%}).",
         "one": "Only {C1%} of participants who received {C1} experienced {T}.",
