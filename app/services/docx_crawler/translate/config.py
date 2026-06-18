e485631404102c7537f9ab76e20f4f8bd34a9371
Oscar Jeffery <oscarjjeff920@gmail.com>
2026-06-18 11:55:42 +0100

    added config file for the translator, initially used just to store the template output sentences

---
+++ app/services/docx_crawler/translate/config.py
@@ -0,0 +1,9 @@
+OUTPUT_SENTENCES = {
+    "totals": "A total of {C1N} participants received {C1}, and a total of {C2N} participants received {C2}.",
+    "per-term": {
+        "both": "More participants who received {C1} ({C1%}) experienced {T} compared to {C2} ({C2%}).",
+        "equal": "An equal proportion of participants experienced {T} ({C1%}).",
+        "one": "Only {C1%} of participants who received {C2} experienced {T}.",
+        "none": "No participants experienced {T}.",
+    },
+}
